b3aab75686f42924c81d4ab45b62edbb0f5914a0
ButteryStancakes <jlj3322@gmail.com>
2025-01-21 20:22:03 -0500

    Control pad + fish skins

--- Plugin.cs
+++ Plugin.cs
@@ -14,10 +14,10 @@ namespace UpturnedVariety
     [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        const string PLUGIN_GUID = "butterystancakes.lethalcompany.upturnedvariety", PLUGIN_NAME = "Upturned Variety", PLUGIN_VERSION = "1.2.0";
+        const string PLUGIN_GUID = "butterystancakes.lethalcompany.upturnedvariety", PLUGIN_NAME = "Upturned Variety", PLUGIN_VERSION = "1.3.0";
         internal static new ManualLogSource Logger;
 
-        internal static ConfigEntry<bool> configGift, configCandy, configPerfume, configPerfumeMeshes, configPills, configMug;
+        internal static ConfigEntry<bool> configGift, configCandy, configPerfume, configPerfumeMeshes, configPills, configMug, configControlPad, configFish;
 
         void Awake()
         {
@@ -65,6 +65,20 @@ namespace UpturnedVariety
                 "Enables alternate palettes for the \"Mug\" item."
             );
 
+            configControlPad = Config.Bind(
+                "Items",
+                "ControlPad",
+                true,
+                "Enables alternate palettes for the \"Control pad\" item."
+            );
+
+            configFish = Config.Bind(
+                "Items",
+                "Fish",
+                true,
+                "Enables alternate models for the \"Plastic fish\" item."
+            );
+
             try
             {
                 AssetBundle upturnedBundle = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "upturnedvariety"));
@@ -91,6 +105,14 @@ namespace UpturnedVariety
                     ItemVariety.pillBottle2 = upturnedBundle.LoadAsset<Texture>("PillBottleTextureB2");
                 if (configMug.Value)
                     ItemVariety.coffeeMug6 = upturnedBundle.LoadAsset<Material>("CoffeeMug6");
+                if (configControlPad.Value)
+                    ItemVariety.controlPad2 = upturnedBundle.LoadAsset<Texture>("ArcadeControlPanel2");
+                if (configFish.Value)
+                {
+                    ItemVariety.fish2 = upturnedBundle.LoadAsset<Mesh>("Fish2");
+                    ItemVariety.sardine = upturnedBundle.LoadAsset<Mesh>("Sardine");
+                    ItemVariety.sardineBanana = upturnedBundle.LoadAsset<Mesh>("Sardine.001");
+                }
                 upturnedBundle.Unload(false);
             }
             catch
@@ -128,6 +150,8 @@ namespace UpturnedVariety
                     }
                 }
             }
+
+            ItemVariety.cache.Clear();
         }
 
         [HarmonyPatch(typeof(RoundManager), nameof(RoundManager.SyncScrapValuesClientRpc))]
@@ -137,7 +161,7 @@ namespace UpturnedVariety
             ItemVariety.offset = -1;
             for (int i = 0; i < spawnedScrap.Length; i++)
             {
-                if (spawnedScrap[i].TryGet(out NetworkObject networkObject) && networkObject.TryGetComponent(out GrabbableObject grabbableObject))
+                if (spawnedScrap[i].TryGet(out NetworkObject networkObject) && ItemVariety.cache.Add(networkObject.NetworkObjectId) && networkObject.TryGetComponent(out GrabbableObject grabbableObject))
                 {
                     if (Plugin.configGift.Value && grabbableObject is GiftBoxItem)
                     {
@@ -222,6 +246,61 @@ namespace UpturnedVariety
                         grabbableObject.mainObjectRenderer.material = ItemVariety.coffeeMug6;
                         Plugin.Logger.LogDebug($"Mug #{networkObject.NetworkObjectId} using alternate texture");
                     }
+                    else if (Plugin.configControlPad.Value && grabbableObject.itemProperties.name == "ControlPad" && ItemVariety.controlPad2 != null && grabbableObject.TryGetComponent(out Renderer rend) && rend.sharedMaterial != null && rend.sharedMaterial.name.StartsWith("ArcadeControlPanel") && ItemVariety.GetSkinIndex(grabbableObject.targetFloorPosition) == 1)
+                    {
+                        rend.material.mainTexture = ItemVariety.controlPad2;
+                        Plugin.Logger.LogDebug($"Controller #{networkObject.NetworkObjectId} using alternate texture");
+                    }
+                    else if (Plugin.configFish.Value && grabbableObject.itemProperties.name == "FishTestProp" && grabbableObject.mainObjectRenderer.TryGetComponent(out MeshFilter mesh))
+                    {
+                        if (ItemVariety.sardineBanana != null && ItemVariety.GetSkinIndex(grabbableObject.targetFloorPosition, 9) == 8)
+                        {
+                            mesh.mesh = ItemVariety.sardineBanana;
+                            Material yellowRubber = grabbableObject.mainObjectRenderer.materials[1];
+                            Material material001 = Object.Instantiate(yellowRubber);
+                            yellowRubber.SetColor("_Color", ItemVariety.banana);
+                            yellowRubber.SetColor("_BaseColor", ItemVariety.banana);
+                            material001.SetColor("_Color", ItemVariety.stem);
+                            material001.SetColor("_BaseColor", ItemVariety.stem);
+                            grabbableObject.mainObjectRenderer.materials = [
+                                grabbableObject.mainObjectRenderer.sharedMaterials[0],
+                                yellowRubber,
+                                material001
+                            ];
+                        }
+                        else if (ItemVariety.fish2 != null && ItemVariety.sardine != null)
+                        {
+                            switch (ItemVariety.GetSkinIndex(grabbableObject.targetFloorPosition, 3))
+                            {
+                                // green
+                                default:
+                                    continue;
+                                // yellow
+                                case 1:
+                                    mesh.mesh = ItemVariety.fish2;
+                                    Material yellowFish = grabbableObject.mainObjectRenderer.materials[1];
+                                    yellowFish.SetColor("_Color", ItemVariety.fishYellow);
+                                    yellowFish.SetColor("_BaseColor", ItemVariety.fishYellow);
+                                    grabbableObject.mainObjectRenderer.materials = [
+                                        yellowFish,
+                                        grabbableObject.mainObjectRenderer.sharedMaterials[0]
+                                    ];
+                                    break;
+                                // red
+                                case 2:
+                                    mesh.mesh = ItemVariety.sardine;
+                                    Material silverFish = grabbableObject.mainObjectRenderer.materials[1];
+                                    silverFish.SetColor("_Color", ItemVariety.fishRed);
+                                    silverFish.SetColor("_BaseColor", ItemVariety.fishRed);
+                                    grabbableObject.mainObjectRenderer.materials = [
+                                        grabbableObject.mainObjectRenderer.sharedMaterials[0],
+                                        silverFish
+                                    ];
+                                    break;
+                            }
+                        }
+                        Plugin.Logger.LogDebug($"Fish #{networkObject.NetworkObjectId} using alternate model");
+                    }
                 }
             }
         }
@@ -230,19 +309,24 @@ namespace UpturnedVariety
     internal class ItemVariety
     {
         internal static AudioClip boombox;
-        internal static Texture giftBoxTex2, pillBottle2;
+        internal static Texture giftBoxTex2, pillBottle2, controlPad2;
         internal static Material lollipop2, lollyStick, coffeeMug6;
         internal static Color perfumeRed = new(0.9f, 0.3922666f, 0.324f, 0.6313726f),
                               perfumeRedTrans = new(1f, 0.4f, 0.4f),
                               perfumeBlue = new(0.4509091f, 0.416f, 0.8f, 0.6313726f), // 0.4076735f, 0.368f, 0.8f
                               perfumeBlueTrans = new(0.59f, 0.6241666f, 1f),
                               perfumeBlack = new(0.01f, 0.01f, 0.01f, 0.6313726f),
-                              perfumeBlackTrans = new(0.48f, 0.48f, 0.48f)/*,
-                              perfumePink = new(0.9f, 0.297f, 0.5985f),
-                              perfumePinkTrans = new(1f, 0.52f, 0.7276597f)*/;
-        internal static Mesh lollyMesh;
+                              perfumeBlackTrans = new(0.48f, 0.48f, 0.48f),
+                              /*perfumePink = new(0.9f, 0.297f, 0.5985f),
+                              perfumePinkTrans = new(1f, 0.52f, 0.7276597f)*/
+                              fishYellow = new(0.773024f, 0.8392157f, 0.3692549f),
+                              fishRed = new(0.5019608f, 0.1780566f, 0.1556078f),
+                              banana = new(0.44f, 0.4296874f, 0.2028124f),
+                              stem = new(0.3799999f, 0.243271f, 0.1527103f);
+        internal static Mesh lollyMesh, fish2, sardine, sardineBanana;
         internal static Mesh[] perfumeMeshes;
 
+        internal static HashSet<ulong> cache = [];
         internal static int offset;
 
         internal static int GetSkinIndex(Vector3 pos, int count = 2)
